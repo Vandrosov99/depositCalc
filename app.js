9338ea4b2d6e9cd74dd2a2184b5d406e80880781
rpk63 <Vandrosov99>
2020-02-06 23:42:40 +0200

    add loader

--- app.js
+++ app.js
@@ -1,59 +1,69 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+    document.getElementById('loading').style.display = 'block';
+    document.getElementById('results').style.display = 'none';
+    setTimeout(calculateResults, 2000);
+    e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e){
-  console.log('Calculating...');
-  // UI Vars
-  const amount = document.getElementById('amount');
-  const interest = document.getElementById('interest');
-  const years = document.getElementById('years');
-  const monthlyPayment = document.getElementById('monthly-payment');
-  const totalPayment = document.getElementById('total-payment');
-  const totalInterest = document.getElementById('total-interest');
+function calculateResults() {
 
-  const principal = parseFloat(amount.value);
-  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
-  const calculatedPayments = parseFloat(years.value) * 12;
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('results').style.display = 'block';
+    console.log('Calculating...');
+    // UI Vars
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
 
-  // Compute monthly payment
-  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-  const monthly = (principal*x*calculatedInterest)/(x-1);
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
 
-  if(isFinite(monthly)) {
-    monthlyPayment.value = monthly.toFixed(2);
-    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
-  } else {
-    showError('Please check your numbers');
-  }
+    // Compute monthly payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
 
-  e.preventDefault();
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    } else {
+        showError('Please check your numbers');
+    }
+
 }
 
 // Show Error
-function showError(error){
-  // Create a div
-  const errorDiv = document.createElement('div');
+function showError(error) {
 
-  // Get elements
-  const card = document.querySelector('.card');
-  const heading = document.querySelector('.heading');
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('results').style.display = 'none';
+    // Create a div
+    const errorDiv = document.createElement('div');
 
-  // Add class
-  errorDiv.className = 'alert alert-danger';
+    // Get elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
 
-  // Create text node and append to div
-  errorDiv.appendChild(document.createTextNode(error));
+    // Add class
+    errorDiv.className = 'alert alert-danger';
 
-  // Insert error above heading
-  card.insertBefore(errorDiv, heading);
+    // Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
 
-  // Clear error after 3 seconds
-  setTimeout(clearError, 3000);
+    // Insert error above heading
+    card.insertBefore(errorDiv, heading);
+
+    // Clear error after 3 seconds
+    setTimeout(clearError, 3000);
 }
 
 // Clear error
-function clearError(){
-  document.querySelector('.alert').remove();
+function clearError() {
+    document.querySelector('.alert').remove();
 }
